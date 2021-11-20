f46335f8d38743c740526d6baf2c9b779c98474c
Vinnie-Windows <vrg6@uakron.edu>
2021-11-20 00:50:11 -0500

    get dragging and naming working

---
+++ resume-site-generator/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace resume_site_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("mousing down");
+            listBox1.DoDragDrop(listBox1.Text, DragDropEffects.Copy | DragDropEffects.Move);
+        }
+
+        private void listBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.Text))
+            {
+                System.Diagnostics.Debug.WriteLine(e.Data.GetData(DataFormats.Text).ToString());
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+        /*
+private void pictureBox1_Click(object sender, EventArgs e)
+{
+System.Diagnostics.Debug.WriteLine("clicked");
+}
+
+private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+{
+System.Diagnostics.Debug.WriteLine("mousing down");
+pictureBox1.DoDragDrop(pictureBox1.Text, DragDropEffects.Copy | DragDropEffects.Move);
+}
+
+private void pictureBox1_MouseEnter(object sender, EventArgs e)
+{
+pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+}
+
+private void textBox1_DragEnter(object sender, DragEventArgs e)
+{
+if (e.Data.GetDataPresent(DataFormats.Bitmap) != true)
+{
+System.Diagnostics.Debug.WriteLine("bbb");
+e.Effect = DragDropEffects.Copy;
+}
+else
+{
+System.Diagnostics.Debug.WriteLine("aaaaa");
+}
+}
+*/
+    }
+}
--- resume-site-generator/Properties/Resources.Designer.cs
+++ resume-site-generator/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DragAndDropSite.Properties
-{
-
-
+namespace resume_site_generator.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,54 @@ namespace DragAndDropSite.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DragAndDropSite.Properties.Resources", typeof(Resources).Assembly);
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("resume_site_generator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ABT97BB5C67A3E0B8C3592C0AEC72F3EA434021DFE4A8D61665B3FD8BC59B7C53AF {
+            get {
+                object obj = ResourceManager.GetObject("ABT97BB5C67A3E0B8C3592C0AEC72F3EA434021DFE4A8D61665B3FD8BC59B7C53AF", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
